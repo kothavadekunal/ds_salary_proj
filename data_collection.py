e3305bb1e3a972f3bce76d085cbf34edd46c7a6a
KK <kothavadekunal@gmail.com>
2020-07-05 16:05:55 +0530

    Uploading data collection files

--- data_collection.py
+++ data_collection.py
@@ -8,4 +8,5 @@ Created on Thu Jul  2 20:24:32 2020
 import scrapper as sc
 import pandas as pd
 
-df = sc.get_jobs("data scientist",15 ,False)
+df = sc.get_jobs("data scientist",1000 ,False)
+df.to_csv('glassdoor_jobs.csv',index = False)
